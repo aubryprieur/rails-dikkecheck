dc6d1f954cf985d218e6fd396f13fad89fada34a
aubry prieur <aubry.prieur@gmail.com>
2017-08-14 19:05:02 +0200

    add model review #5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814153120) do
+ActiveRecord::Schema.define(version: 20170814170052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20170814153120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_belges_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.bigint "belge_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["belge_id"], name: "index_reviews_on_belge_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +61,6 @@ ActiveRecord::Schema.define(version: 20170814153120) do
   end
 
   add_foreign_key "belges", "users"
+  add_foreign_key "reviews", "belges"
+  add_foreign_key "reviews", "users"
 end
