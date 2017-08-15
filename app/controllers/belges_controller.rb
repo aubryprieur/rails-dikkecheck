cf0de15d6daaeb0eed4bb3886f9b8a1c91310c24
guillaume trupin <gtrupin@gmail.com>
2017-08-15 12:32:22 +0200

    Config controller belges

--- app/controllers/belges_controller.rb
+++ app/controllers/belges_controller.rb
@@ -1,22 +1,51 @@
 class BelgesController < ApplicationController
+  before_action :set_belge, only: [:show, :edit, :update, :destroy]
   def index
+    @belges = Belge.all
   end
 
   def show
   end
 
   def new
+    @belge = Belge.new
   end
 
   def create
+    @belge = Belge.new(belge_params)
+       if @belge.save
+        redirect_to belge_path(@belge)
+       else
+        render :new
+      end
   end
 
   def edit
   end
 
   def update
+    if @belge.update(belge_params)
+        redirect_to belge_path(@belge)
+       else
+        render :new
+      end
   end
 
   def destroy
+    if @belge.destroy
+        redirect_to belges_path
+       else
+        render :new
+      end
+  end
+
+  private
+
+  def set_belge
+      @belge = Belge.find(params[:id])
+    end
+
+  def belge_params
+    params.require(:belge).permit(:first_name, :last_name, :description, :photo, :price, :user_id)
   end
 end
