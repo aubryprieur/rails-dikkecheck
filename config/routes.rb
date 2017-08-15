48e7554ad8e15de55ec31380757a6547ddcfcd0d
guillaume trupin <gtrupin@gmail.com>
2017-08-15 11:17:42 +0200

    Création des controlleurs users belges et bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+
+  get 'bookings/show'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/edit'
+
+  get 'bookings/update'
+
+  get 'bookings/destroy'
+
+  get 'belges/index'
+
+  get 'belges/show'
+
+  get 'belges/new'
+
+  get 'belges/create'
+
+  get 'belges/edit'
+
+  get 'belges/update'
+
+  get 'belges/destroy'
+
   resources :users
 
   devise_for :users
