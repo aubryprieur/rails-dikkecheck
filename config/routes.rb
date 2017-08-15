57163b6202820228b22792dcc1d1431efa1ae1c8
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-08-15 11:40:06 +0200

    supression users-controller et users views + modification des routes en resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,77 +1,17 @@
 Rails.application.routes.draw do
-  get 'categories/index'
+  resources :categories
 
-  get 'categories/show'
+  resources :belge_categories
 
-  get 'categories/new'
+  resources :reviews
 
-  get 'categories/create'
+  resources :bookings
 
-  get 'categories/edit'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
-
-  get 'belge_categories/index'
-
-  get 'belge_categories/show'
-
-  get 'belge_categories/new'
-
-  get 'belge_categories/create'
-
-  get 'belge_categories/edit'
-
-  get 'belge_categories/update'
-
-  get 'belge_categories/destroy'
-
-  get 'reviews/index'
-
-  get 'reviews/show'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'bookings/index'
-
-  get 'bookings/show'
-
-  get 'bookings/new'
-
-  get 'bookings/create'
-
-  get 'bookings/edit'
-
-  get 'bookings/update'
-
-  get 'bookings/destroy'
-
-  get 'belges/index'
-
-  get 'belges/show'
-
-  get 'belges/new'
-
-  get 'belges/create'
-
-  get 'belges/edit'
-
-  get 'belges/update'
-
-  get 'belges/destroy'
-
-  resources :users
+  resources :belges
 
   devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
